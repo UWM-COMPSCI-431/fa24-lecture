15210be889cd32507c3048c14c467bb791bdd034
John Boyland <boyland@uwm.edu>
2024-11-11 08:48:31 -0600

    Added relations

---
+++ relations.pl
@@ -0,0 +1,13 @@
+parent(patricia,anna).
+parent(michael,anna).
+parent(virginia,patricia).
+parent(virginia,ericka).
+parent(ericka,sonya).
+parent(ericka,corina).
+parent(robert,sonya).
+parent(robert,corina).
+parent(jack,patricia).
+parent(jack,ericka).
+parent(anna,kimberly).
+parent(sonya,alexandra).
+parent(patrick,alexandra).
